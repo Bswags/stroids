d83bc5c59175744dfea5066443de713998a638e6
Brian <bhw2113@columbia.edu>
2014-02-06 19:50:10 -0800

    added background and ship sprites

--- Stroids/Stroids/MyScene.m
+++ Stroids/Stroids/MyScene.m
@@ -8,22 +8,34 @@
 
 #import "MyScene.h"
 
+static const uint32_t shipCategory = 0x1 << 0;
+static const uint32_t obstacleCategory = 0x1 << 1;
+
+static const float BG_VELOCITY = 100.0;
+static inline CGPoint CGPointAdd(const CGPoint a, const CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+static inline CGPoint CGPointScale(const CGPoint a, const CGFloat b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
 @implementation MyScene
+
+SKSpriteNode *ship;
+SKAction *actionMoveLeft;
+SKAction *actionMoveRight;
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor whiteColor];
+        [self initializeBackground];
+        [self addShip];
+        // Make self delegate of physics world
+        self.physicsWorld.gravity = CGVectorMake(0, 0);
+        //self.physicsWorld.contactDelegate = self;
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
     }
     return self;
 }
@@ -31,18 +43,50 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLoc = [touch locationInNode:self.scene];
+    
+    if (touchLoc.y > ship.position.y) {
+        if (ship.position.y < 300)
+            [ship runAction:actionMoveLeft];
+    } else {
+        if (ship.position.y > 50)
+            [ship runAction:actionMoveRight];
+    }
+
+}
+
+-(void)addShip {
+    // Initialize spaceship
+    SKSpriteNode *ship = [SKSpriteNode new];
+    ship = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship.png"];
+    [ship setScale:0.25];
+    ship.zRotation = -M_PI / 2;
+    
+    // Add the fizzicks
+    ship.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ship.size];
+    ship.physicsBody.categoryBitMask = shipCategory;
+    ship.physicsBody.dynamic = YES;
+    ship.physicsBody.contactTestBitMask = obstacleCategory;
+    ship.physicsBody.collisionBitMask = 0;
+    ship.name = @"ship";
+    ship.position = CGPointMake(160, 120);
+    
+    // Add ship to scene
+    [self addChild:ship];
+    
+    // Create actions
+    actionMoveLeft = [SKAction moveByX:0 y:30 duration:0.2];
+    actionMoveRight = [SKAction moveByX:0 y:-30 duration:0.2];
+}
+
+-(void)initializeBackground {
+    for (int i = 0; i < 2; i++) {
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"bg.png"];
+        bg.position = CGPointMake(i * bg.size.width, 0);
+        bg.anchorPoint = CGPointZero;
+        bg.name = @"bg";
+        [self addChild:bg];
     }
 }
 
