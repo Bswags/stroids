c9c6860ae188d6dd32aec75bd38d018b22318d43
Brian <bhw2113@columbia.edu>
2014-02-06 17:25:41 -0800

    project template created

---
+++ Stroids/Stroids/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  Stroids
+//
+
+//  Copyright (c) 2014 brianwagner. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ Stroids/Stroids/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Stroids
+//
+
+//  Copyright (c) 2014 brianwagner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
